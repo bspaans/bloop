806777d63db0a99e7fda4c506c52de74a11dffed
Bart Spaans <bart.spaans@gmail.com>
2022-04-19 02:28:10 +0100

    Add white noise generator

--- bloop.c
+++ bloop.c
@@ -47,6 +47,22 @@ bloop_generator *bloop_sine_wave(bloop_generator *pitch, bloop_generator *gain) 
     return bloop_new_generator(bloop_sine_wave_, v);
 }
 
+typedef struct bloop_white_noise_data {
+    bloop_generator *gain;
+} bloop_white_noise_data;
+
+float bloop_white_noise_(void *value, int tick) {
+    bloop_white_noise_data *data = (bloop_white_noise_data *) value;
+    float v = (float)rand()/(float)(RAND_MAX/2.0) - -1;
+    return v * bloop_run(data->gain, tick);
+}
+
+bloop_generator *bloop_white_noise(bloop_generator *gain) {
+    bloop_white_noise_data *v = malloc(sizeof(*v));
+    v->gain = gain;
+    return bloop_new_generator(bloop_white_noise_, v);
+}
+
 float bloop_constant_(void *value, int tick) {
     float *v = (float *)value;
     return *v;
@@ -211,7 +227,7 @@ bloop_generator *bloop_delay(bloop_generator *input, int delay_samples, float fa
     v->factor = factor;
     v->feedback = feedback;
     v->ring_index = 0;
-    v->ring = malloc(sizeof(float) * delay_samples);
+    v->ring = malloc(sizeof(float) * delay_samples); // TODO: always allocate a max size and make time controllable?
     return bloop_new_generator(bloop_delay_, v);
 }
 
@@ -296,9 +312,11 @@ void init(void) {
         .stream_cb = stream_cb
     });
     generator = bloop_sine_wave(LFO(1.0, 440.0, 110.0), C(1.0)); 
+    bloop_generator *kick_drum_hit = bloop_white_noise(bloop_adsr(0.3, 0.0, 150, 150, 0, 0));
     bloop_generator *kick_drum = bloop_distortion(bloop_sine_wave(bloop_interpolation(90, 36, 4000), bloop_adsr(1.0, 0.2, 500, 500, 4000, 2000)), bloop_interpolation(0.9, 0.2, 100), C(1.0));
-    bloop_generator *kick_drum_rumble1 = bloop_delay(kick_drum, 22050, 0.8, 0.1);
-    bloop_generator *kick_drum_rumble2 = bloop_repeat(bloop_distortion(bloop_delay(bloop_average(2, kick_drum, kick_drum_rumble1), 11025, 0.7, 0.8), C(0.8), C(4.5)), 88200);
+    bloop_generator *kick_drum1 = bloop_average(2, kick_drum, kick_drum_hit);
+    bloop_generator *kick_drum_rumble1 = bloop_delay(kick_drum1, 22050, 0.8, 0.1);
+    bloop_generator *kick_drum_rumble2 = bloop_repeat(bloop_distortion(bloop_delay(bloop_average(2, kick_drum, kick_drum_rumble1), 11025, 0.7, 0.0), C(0.8), C(4.5)), 88200);
     bloop_generator *wobble2 = bloop_sine_wave(bloop_lfo(LFO(8.0, 24, 24), C(880), C(440.0)), bloop_lfo(C(128.0), C(0.2), LFO(2, 0.1, 0.05)));
     generator = bloop_average(2, kick_drum_rumble2, wobble2);
     sg_setup(&(sg_desc){
@@ -333,5 +351,3 @@ sapp_desc sokol_main(int argc, char* argv[]) {
     };
 }
 
-
-// TODO: delay, white noise, controlled pitch
